811d005c77c102d33ff7aa0d10f4294fd0bab87d
Parakram <89983841+Parakramk@users.noreply.github.com>
2023-11-23 09:50:11 +0545

    add base code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <cpr/cpr.h>
 #include <iostream>
-#include<boost/json/src.hpp>
-#include<dotenv.h>
-#include<string>
+#include <boost/json/src.hpp>
+#include <dotenv.h>
+#include <string>
+#include <regex>
+
 
 struct tokenResult {
     bool success;
     std::string token;
 };
+
+struct authHeader {
+    std::string auth;
+    std::string value;
+};
+
+std::map<std::string, std::string> isValidType(const std::string& url) {
+    std::regex urlPattern("https://open\\.spotify\\.com/(track|album)/([a-zA-Z0-9]+)");
+    std::smatch match;
+
+    if (std::regex_match(url, match, urlPattern)) {
+        return { {"type", match[1]}, {"id", match[2]} };
+    }
+
+    return { {"type", "0"}, {"id", "0"} };
+}
 
 tokenResult getToken() {
     const std::string clientId = std::getenv("CLIENT_ID");
@@ -27,14 +45,33 @@ tokenResult getToken() {
     return { true, accessToken };
 }
 
+authHeader getAuthHeader(const std::string& token) {
+    return { "authorization" , "Bearer " + token };
+}
+
+std::map<std::string, std::string> inputURL() {
+    std::string url;
+    std::cout << "Enter Spotify URL: ";
+    std::cin >> url;
+    std::map<std::string, std::string> result = isValidType(url);
+    if (result["type"] != "0") {
+        std::cout << "URL is valid." << std::endl;
+        return { {"type",result["type"]}, {"id", result["id"]} };
+    }
+    return inputURL();
+}
+
+
 int main() {
-    // Call the function
-    dotenv::init();
+    dotenv::init(); // Load Environment Variables from .env
+    inputURL();
     tokenResult tokenResult = getToken();
 
     // Check the result
     if (tokenResult.success) {
-        std::cout << "Token: " << tokenResult.token << std::endl;
+        // std::cout << "Token: " << tokenResult.token << std::endl;
+        authHeader res = getAuthHeader(tokenResult.token);
+        std::cout << "{" << res.auth << ":" << res.value << std::endl;
     }
     else {
         std::cerr << "Error: " << tokenResult.token << std::endl;
